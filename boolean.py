ba9dd671726a5201afc946923338a8b1675d5adf
Matthew Hartley <mhartley@cantab.net>
2013-07-17 17:10:34 +0100

    Plotting state space diagram

--- boolean.py
+++ boolean.py
@@ -1,4 +1,5 @@
 import itertools
+import pygraphviz as pgv
 from pprint import pprint
 
 class BooleanNetwork(object):
@@ -21,8 +22,17 @@ class BooleanNetwork(object):
 def up3(current_state):
     new_state = [0] * len(current_state)
 
-    new_state[0] = current_state[1]
+    new_state[0] = int(not(current_state[1]))
     new_state[1] = int(current_state[0] or current_state[2])
+    new_state[2] = current_state[1]
+
+    return tuple(new_state)
+
+def up3_and(current_state):
+    new_state = [0] * len(current_state)
+
+    new_state[0] = int(not(current_state[1]))
+    new_state[1] = int(current_state[0] and current_state[2])
     new_state[2] = current_state[1]
 
     return tuple(new_state)
@@ -41,10 +51,19 @@ def all_states(n):
     state_it = itertools.product(*sl)
     return state_it
 
+def draw_state_map(update_rule, size, output_filename):
+    state_map = {s: update_rule(s) for s in all_states(size)}
+    G = pgv.AGraph(directed=True)
+    [G.add_node(k) for k in state_map]
+    [G.add_edge(k, v) for k, v in state_map.items()]
+    G.layout(prog='dot')
+    G.draw(output_filename)
+    
 def main():
     bn = BooleanNetwork(3, [0, 0, 1])
-    state_map = {s: up3(s) for s in all_states(3)}
-    pprint(state_map)
+    output_filename = 'or.png'
+    draw_state_map(up3, 3, output_filename)
+    draw_state_map(up3_and, 3, 'and.png')
 
 if __name__ == '__main__':
     main()
